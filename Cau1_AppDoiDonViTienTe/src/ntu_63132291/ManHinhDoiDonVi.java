df1674d790480155e092f537f2444b523ba28dd8
duongthithanhmy <152678626+duongthithanhmy@users.noreply.github.com>
2024-03-24 21:42:47 +0700

    Update ManHinhDoiDonVi.java

--- Cau1_AppDoiDonViTienTe/src/ntu_63132291/ManHinhDoiDonVi.java
+++ Cau1_AppDoiDonViTienTe/src/ntu_63132291/ManHinhDoiDonVi.java
@@ -16,12 +16,14 @@ import javax.swing.JCheckBox;
 import java.awt.Choice;
 import java.awt.TextArea;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ManHinhDoiDonVi extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField txtNhap;
 	private JTextField txtKetQua;
 
 	
@@ -35,37 +37,62 @@ public class ManHinhDoiDonVi extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel txtNhap = new JLabel("Nhập VNĐ");
-		txtNhap.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		txtNhap.setBounds(49, 67, 117, 38);
-		contentPane.add(txtNhap);
+		JLabel lblNhap = new JLabel("Nhập VNĐ");
+		lblNhap.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNhap.setBounds(49, 67, 117, 38);
+		contentPane.add(lblNhap);
 		
-		textField = new JTextField();
-		textField.setBounds(190, 67, 340, 38);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtNhap = new JTextField();
+		txtNhap.setBounds(190, 67, 340, 38);
+		contentPane.add(txtNhap);
+		txtNhap.setColumns(10);
 		
 		JButton btnUsd = new JButton("USD");
+		btnUsd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HamDoiUsd();
+			}
+		});
 		btnUsd.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnUsd.setBounds(59, 181, 87, 38);
 		contentPane.add(btnUsd);
 		
 		JButton btnEur = new JButton("EUR");
+		btnEur.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HamDoiEur();
+			}
+		});
 		btnEur.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnEur.setBounds(172, 181, 87, 38);
 		contentPane.add(btnEur);
 		
 		JButton btnNdt = new JButton("NDT");
+		btnNdt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HamDoiNdt();
+			}
+		});
 		btnNdt.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnNdt.setBounds(283, 181, 87, 38);
 		contentPane.add(btnNdt);
 		
 		JButton btnYen = new JButton("YÊN");
+		btnYen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HamDoiDongYen();
+			}
+		});
 		btnYen.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnYen.setBounds(396, 181, 87, 38);
 		contentPane.add(btnYen);
 		
 		JButton btnWon = new JButton("WON");
+		btnWon.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HamDoiDongWon();
+			}
+		});
 		btnWon.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnWon.setBounds(513, 181, 87, 38);
 		contentPane.add(btnWon);
@@ -81,5 +108,15 @@ public class ManHinhDoiDonVi extends JFrame {
 		txtKetQua.setColumns(10);
 		txtKetQua.setBounds(190, 258, 340, 38);
 		contentPane.add(txtKetQua);
+	}
+	void HamDoiUsd() {
+		//lấy dữ liệu từ điều khiển
+		String str_VND = txtNhap.getText();
+		//tỉ giá hoán đổi
+		double change = 23000;
+		//chuyển kiểu
+		Double VND = Double.parseDouble(str_VND);
+		//tính toán
+		
 	}
 }
