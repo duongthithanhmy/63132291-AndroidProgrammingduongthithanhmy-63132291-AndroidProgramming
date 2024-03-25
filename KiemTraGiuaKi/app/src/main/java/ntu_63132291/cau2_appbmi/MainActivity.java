c49228137ce9abd028e1bb8bf82c04b64c33435e
duongthithanhmy <152678626+duongthithanhmy@users.noreply.github.com>
2024-03-25 21:59:18 +0700

    Hàm xử lý

--- KiemTraGiuaKi/app/src/main/java/ntu_63132291/cau2_appbmi/MainActivity.java
+++ KiemTraGiuaKi/app/src/main/java/ntu_63132291/cau2_appbmi/MainActivity.java
@@ -1,6 +1,7 @@
 package ntu_63132291.cau2_appbmi;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    public void HamXuLyBMI(){
+        //Tham chiếu
+        EditText h = findViewById(R.id.editH);
+        EditText w = findViewById(R.id.editW);
+        EditText kq = findViewById(R.id.editKQ);
+
+        String strh = h.getText().toString();
+        String strw = w.getText().toString();
+
+        int height =   Integer.parseInt(strh);
+        int weight =   Integer.parseInt(strw);
+
+        int BMI = weight / (height * height);
+        String strBMI = String.valueOf(BMI); //Chuyển sang dạng chuỗi
+        //Hiện ra màn hình(Lấy dữ liệu hiện ra màn hình)
+        kq.setText(strBMI);
     }
 }
