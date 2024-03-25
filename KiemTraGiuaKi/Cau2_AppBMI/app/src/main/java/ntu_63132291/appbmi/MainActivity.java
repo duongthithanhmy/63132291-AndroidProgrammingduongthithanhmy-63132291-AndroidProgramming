7ff138adf0a574862e7af344c2c3cabd1e06fb11
duongthithanhmy <152678626+duongthithanhmy@users.noreply.github.com>
2024-03-26 03:04:02 +0700

    Update BMI(hàm xử lý)

--- KiemTraGiuaKi/Cau2_AppBMI/app/src/main/java/ntu_63132291/appbmi/MainActivity.java
+++ KiemTraGiuaKi/Cau2_AppBMI/app/src/main/java/ntu_63132291/appbmi/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu_63132291.appbmi;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void HamXuLyBMI() {
+        //Tham chiếu
+        EditText h = findViewById(R.id.editH);
+        EditText w = findViewById(R.id.editW);
+        EditText kq = findViewById(R.id.editKQ);
+        //Lấy dữ liệu về
+        String strh = h.getText().toString();
+        String strw = w.getText().toString();
+        //Chuyển sang dạng số
+        double height = Double.parseDouble(strh);
+        double weight = Double.parseDouble(strw);
+        //Tính toán
+        double BMI = weight / (height * height);
+        //Chuyển sang dạng chuỗi
+        String strBMI = String.format("%.2f", BMI);
+        //Hiện ra màn hình(Lấy dữ liệu hiện ra màn hình)
+        kq.setText(strBMI);
     }
 }
