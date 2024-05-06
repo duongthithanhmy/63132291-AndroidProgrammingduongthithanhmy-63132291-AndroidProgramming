2e7f5a140c7054dc6ab62999d37a91c2c75d4dc5
duongthithanhmy <152678626+duongthithanhmy@users.noreply.github.com>
2024-05-06 16:07:35 +0700

    Update code

--- KiemTraGiuaKi/simpleexplicitintents/app/src/main/java/my/example/simpleexplicitintents/SubActivityOne.java
+++ KiemTraGiuaKi/simpleexplicitintents/app/src/main/java/my/example/simpleexplicitintents/SubActivityOne.java
@@ -1,6 +1,8 @@
 package my.example.simpleexplicitintents;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class SubActivityOne extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    //Hàm đáp ứng sự kiện nhấn lên nút "Quay v trang chủ"
+    //Xử lý chuyển về màn hình trang chủ
+    public  void QuayVe(View v) {
+        //Tạo một đối tượng Intent
+        //Tham số thứ 2 của hàm tạo này, là tên Activity (màn hình) ta muốn chuyển sang
+        Intent iManHinhChinh = new Intent(this, MainActivity.class);
+        //Thực hiện chuyển
+        startActivity(iManHinhChinh);
     }
 }
