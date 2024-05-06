895a8cc528ceacb31f2de51b520ee5d840e1ad05
duongthithanhmy <152678626+duongthithanhmy@users.noreply.github.com>
2024-05-06 16:01:57 +0700

    Cập nhập

--- KiemTraGiuaKi/simpleexplicitintents/app/src/main/java/my/example/simpleexplicitintents/SubActivityOne.java
+++ KiemTraGiuaKi/simpleexplicitintents/app/src/main/java/my/example/simpleexplicitintents/SubActivityOne.java
@@ -1,8 +1,6 @@
 package my.example.simpleexplicitintents;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SubActivityOne extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub_one);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    //Hàm đáp ứng xự kiện nhấn lên nút "Sang màn hình khác"
-    //Xử lý chuyển màn hình
-    public void ChuyenManHinh(View v) {
-        //Tạo một đối tượng Intent
-        //Tham số thứ 2 của hàm này, là tên Activity (màn hình) ta muốn chuyển sang
-        Intent iManHinhKhac = new Intent(this,SubActivityOne.class);
-        //Thực hiện chuyển
-        startActivity(iManHinhKhac);
     }
 }
